4d6b2a1c3152206dcba94caddbe4042197b57d3e
Yuan-Man <68322456+Yuan-ManX@users.noreply.github.com>
2025-05-09 14:46:59 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,1 +1,11 @@
+from .nodes import PlayGame
 
+NODE_CLASS_MAPPINGS = {
+    "PlayGame": PlayGame,
+}
+
+NODE_DISPLAY_NAME_MAPPINGS = {
+    "PlayGame": "Play Game",
+} 
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
